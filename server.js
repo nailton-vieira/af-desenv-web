6334080a19964c794c39257a90d95d4f4d9bc57a
RoberioLeal2 <149443396+RoberioLeal2@users.noreply.github.com>
2024-05-30 09:27:55 -0300

    Adicionando atualização do server ao repo

--- server.js
+++ server.js
@@ -4,3 +4,31 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
+mongoose.connect("mongodb://127.0.0.1:27017/employee");
+
+app.post("/login", (req, res) => {
+    const {email, password} = req.body;
+    EmployeeModel.findOne({email : email})
+    .then(user => {
+        if(user) {
+            if(user.password === password){
+                res.json("Success")
+            }else{
+                res.json("The password is incorrect")
+            }
+        }else{
+            res.json("No record existed")
+        }
+    })
+})
+
+app.post("/register", (req, res) => {
+    EmployeeModel.create(req.body)
+    .then(employees => res.json(employees))
+    .catch(err => res.json(err))
+})
+
+
+app.listen(3001, () => {
+    console.log("server is running")
+})
