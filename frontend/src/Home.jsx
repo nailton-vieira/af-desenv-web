7b2c1aede0b8d22c41ea5c376c8cda5402e31d84
nailton-vieira <nailtonvieira@gmail.com>
2024-05-31 18:41:28 -0300

    Adicionado o frontend ao repo

---
+++ frontend/src/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home(){
+    return(
+        <center>
+            <h1>
+                SEJA BEM VINDO A NOSSA HOMEPAGE
+            </h1>
+        </center>
+    )
+}
+
+export default Home;
